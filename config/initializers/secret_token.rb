78ecd5ceb8564094b71bd82748fbc36d68101856
Chase Gittisarn <chaseofchase@yahoo.com>
2014-01-19 19:25:48 -0800

    Created Rails Application Manager, Odot.
    Created Odot. Added gems, "rspec" and "capybara".

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Odot::Application.config.secret_key_base = '8026cdc0b6002b2c109d0a3ac6c48166b354ad68e5082f723b6818035c9f0cce521d933d26995c790964251203cf111c1fafd5db683fcfd1badf78d2c413d783'
